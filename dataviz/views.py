0cd76cae9712a75a1a7d961d6174b3022a2d42ee
mike.bochenek@gmail.com <mike.bochenek@gmail.com>
2020-06-26 21:41:46 +0200

    starting on upload view and handler

--- dataviz/views.py
+++ dataviz/views.py
@@ -1,4 +1,23 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django import forms
+
+class UploadFileForm(forms.Form):
+    title = forms.CharField(max_length=50)
+    file = forms.FileField()
 
 def index(request):
     return render(request, 'index.html')
+
+def upload(request):
+    return render(request, 'upload.html')
+
+def upload_file(request):
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            handle_uploaded_file(request.FILES['file'])
+            return HttpResponseRedirect('/success/url/')
+    else:
+        form = UploadFileForm()
+    return render(request, 'upload.html', {'form': form})
